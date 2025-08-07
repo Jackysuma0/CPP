ba05da9b5dd2b61de3432a0e2e166a686b3bcabe
gbakulin <galbakulina@gmail.com>
2025-08-07 18:06:11 +0200

    terrible

--- CPP01/ex00/megaphone.cpp
+++ CPP01/ex00/megaphone.cpp
@@ -2,12 +2,22 @@
 
 int main(int argc, char **argv)
 {
-    (void)argv;
-    if (argc == 2)
-        std::cout << "SHHHHH... I THINK THE STUDENTS ARE ASLEEP...\n";
-    if (argc == 4)
-        std::cout << "DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.\n";
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
     if (argc == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+    while(i < argc)
+    {
+        j = 0;
+        while(argv[i][j] != '\0')
+        {
+            std::cout << (char) std::toupper(argv[i][j]);
+            j++;
+        }
+        i++;
+    }
     return (0);
 }
